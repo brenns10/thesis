090dffbde4cca03ac14bb126c5feb3d587a8ff22
Stephen Brennan <smb196@case.edu>
2017-01-17 15:26:47 -0500

    Update the detour daemon to infer client IP, and to clean up iptables rules.

--- src/mproxy.py
+++ src/mproxy.py
@@ -12,40 +12,105 @@ import os
 import socket
 import struct
 
+# See doc/TERMS.md for definitions of variable names.
 SNAT_COMMAND = (
-    'iptables -t nat -A POSTROUTING -s {CLIENT_IP} -d {SERVER_IP} -p tcp '
-    '--dport {SERVER_PORT} -j SNAT --to 192.168.0.6'
+    'iptables -t nat -A POSTROUTING -s {client_ip} -d {server_ip} -p tcp '
+    '--dport {server_port} -j SNAT --to {detour_ip}'
+)
+SNAT_REMOVE_COMMAND = (
+    'iptables -t nat -D POSTROUTING -s {client_ip} -d {server_ip} -p tcp '
+    '--dport {server_port} -j SNAT --to {detour_ip}'
 )
 DNAT_COMMAND = (
-    'iptables -t nat -A PREROUTING -s {CLIENT_IP} -d 192.168.0.6 -p tcp --dport {DETOUR_PORT} '
-    '-j DNAT --to {SERVER_IP}:{SERVER_PORT}'
+    'iptables -t nat -A PREROUTING -s {client_ip} -d {detour_ip} -p tcp'
+    '--dport {detour_port} -j DNAT --to {server_ip}:{server_port}'
 )
-REQUEST_FORMAT = '!4sH4sH'
+DNAT_REMOVE_COMMAND = (
+    'iptables -t nat -D PREROUTING -s {client_ip} -d {detour_ip} -p tcp'
+    '--dport {detour_port} -j DNAT --to {server_ip}:{server_port}'
+)
+REQUEST_FORMAT = '!4sHH'
 
 
-def handle_request(data):
-    request = struct.unpack(REQUEST_FORMAT, data)
-    info = {
-        'CLIENT_IP': socket.inet_ntoa(request[0]),
-        'DETOUR_PORT': request[1],
-        'SERVER_IP': socket.inet_ntoa(request[2]),
-        'SERVER_PORT': request[3],
-    }
-    os.system(SNAT_COMMAND.format(**info))
-    os.system(DNAT_COMMAND.format(**info))
-
-
-def main(port=45672):
-    # ensure that we are configured to forward packets
-    os.system('sysctl -w net.ipv4.ip_forward=1')
+def myip():
+    # An unfortunate hack for getting a local IP address.  This will return the
+    # address of *SOME* interface that can route us to 8.8.8.8, but not
+    # necessarily the *ONLY* interface that we have. This means that if this
+    # runs on a computer which actually has multiple interfaces and receives
+    # packets on more than one of them, I'll have to modify this. But for now,
+    # it will work.
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('0.0.0.0', port))
-    msg_size = struct.calcsize(REQUEST_FORMAT)
-    while True:
-        data = s.recv(msg_size)
-        assert(len(data) == msg_size)
-        handle_request(data)
+    s.connect(('8.8.8.8', 53))
+    addr = s.getsockname()[0]
+    s.close()
+    return addr
+
+
+class MProxy(object):
+
+    def __init__(self, port=45672, ip=None):
+        """Simple class that manages IPTables rules for a NAT proxy."""
+        self._port = port
+        self._entries = set()
+        self._ip = ip if ip else myip()
+
+    def add_rules(self, info):
+        """Add IPTables rule and record a request."""
+        os.system(SNAT_COMMAND.format(**info, detour_ip=self._ip))
+        os.system(DNAT_COMMAND.format(**info, detour_ip=self._ip))
+        self._entries.add(self.freeze(info))
+
+    def del_rules(self, info):
+        """Delete IPTables rule and recorded information."""
+        os.system(SNAT_REMOVE_COMMAND.format(**info, detour_ip=self._ip))
+        os.system(DNAT_REMOVE_COMMAND.format(**info, detour_ip=self._ip))
+        self._entries.remove(self.freeze(info))
+
+    def freeze(self, info):
+        """Freeze a dict (i.e. turn it into a hashable object)"""
+        return frozenset(info.items())
+
+    def thaw(self, tup):
+        """Thaw a dict (i.e. turn it back into a dict)"""
+        return dict(tup)
+
+    def contains_entry(self, info):
+        """Return True if we've already created a rule for this request."""
+        return self.freeze(info) in self._entries
+
+    def handle_request(self, data, addr):
+        """Handle an incoming UDP request."""
+        request = struct.unpack(REQUEST_FORMAT, data)
+        info = {
+            'server_ip': socket.inet_ntoa(request[0]),
+            'server_port': request[1],
+            'detour_port': request[2],
+            'client_ip': addr[0],
+        }
+        if not self.contains_entry(info):
+            self.add_rules(info)
+
+    def clean_up(self):
+        """Delete all rules we have created."""
+        for frozen in self._entries:
+            self.del_rules(self.thaw(frozen))
+
+    def serve(self):
+        """Bind this server to a port and serve forever."""
+        # ensure that we are configured to forward packets
+        os.system('sysctl -w net.ipv4.ip_forward=1')
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind(('0.0.0.0', self._port))
+        msg_size = struct.calcsize(REQUEST_FORMAT)
+        try:
+            while True:
+                data, addr = s.recv(msg_size)
+                assert(len(data) == msg_size)
+                self.handle_request(data, addr)
+        finally:
+            # No matter what, we want to delete the IPTables rules we created.
+            self.clean_up()
 
 
 if __name__ == '__main__':
-    main()
+    MProxy().serve()
--- src/mproxy_client.py
+++ src/mproxy_client.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """
-Send requests to the mproxy daemon.
+Send requests to the mproxy daemon. Must run from client.
 
-usage: mproxy_client.py DAEMON_IP CLIENT_IP DETOUR_PORT SERVER_IP SERVER_PORT
+usage: mproxy_client.py DAEMON_IP SERVER_IP SERVER_PORT DETOUR_PORT
 """
 
 import sys
@@ -12,14 +12,14 @@ import socket
 REQUEST_FORMAT = '!4sH4sH'
 
 def main():
-    if len(sys.argv) != 6:
-        print('requires 5 args...')
+    if len(sys.argv) != 5:
+        print('usage: mproxy_client.py DAEMON_IP SERVER_IP SERVER_PORT DETOUR_PORT')
+        sys.exit(1)
     daemon_ip = sys.argv[1]
     request = sys.argv[2:]
-    request[0] = socket.inet_aton(request[0])
+    request[0] = socket.inet_aton(request[2])
     request[1] = int(request[1])
-    request[2] = socket.inet_aton(request[2])
-    request[3] = int(request[1])
+    request[2] = int(request[1])
     data = struct.pack(REQUEST_FORMAT, *request)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.sendto(data, (daemon_ip, 45672))
