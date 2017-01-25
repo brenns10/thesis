bd6864705ece1c2b4063c7c2c043daf0d3d0acd6
Stephen Brennan <smb196@case.edu>
2017-01-24 23:30:58 -0500

    Stop relying on simple send and actually send data

--- src/netlink_client.c
+++ src/netlink_client.c
@@ -7,21 +7,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <netlink/netlink.h>
+#include <netlink/attr.h>
 #include <netlink/genl/genl.h>
 #include <netlink/genl/ctrl.h>
 
-#define DETOUR_FAMILY "DETOUR"
-#define DETOUR_VERSION 1
-
 /*
- * print usage information and exit
+ * The following are shamelessly taken from the kernel code.
  */
-void usage(char *name)
-{
-	fprintf(stderr, "usage: %s COMMAND_NUM\n", name);
-	fprintf(stderr, "Send a message to the MPTCP detour kernel module.\n");
-	exit(EXIT_FAILURE);
-}
+#define DETOUR_FAMILY "DETOUR"
+#define DETOUR_C_ECHO 1
+#define DETOUR_VERSION 1
+#define DETOUR_A_MSG 1
 
 /*
  * Uses libnl to send an empty message to the given family or command.
@@ -29,16 +25,8 @@ void usage(char *name)
 int main(int argc, char *argv[])
 {
 	int family;
-	uint8_t cmd;
 	struct nl_sock *sk;
 	int rc;
-
-	if (argc < 2)
-		usage(argv[0]);
-
-	cmd = (uint8_t)atoi(argv[1]);
-	if (!cmd)
-		fprintf(stderr, "cmd was 0, may be error...\n");
 
 	sk = nl_socket_alloc();
 	if (!sk) {
@@ -62,10 +50,26 @@ int main(int argc, char *argv[])
 		printf("resolved family %s to %d\n", DETOUR_FAMILY, family);
 	}
 
-	rc = genl_send_simple(sk, family, cmd, DETOUR_VERSION, 0);
-	if (!rc)
-		fprintf(stderr, "genl_send_simple failed: %d\n", rc);
+	struct nl_msg *msg = nlmsg_alloc();
+	if (!msg) {
+		fprintf(stderr, "nlmsg_alloc() returned NULL\n");
+		rc = EXIT_FAILURE;
+		goto exit;
+	}
+	//               msg, port,         seq,      family, hdrlen, flags, cmd
+	if (!genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, family, 0, 0,
+	                 DETOUR_C_ECHO, DETOUR_VERSION)) {
+		fprintf(stderr, "genlmsg_put() failed\n");
+		rc = EXIT_FAILURE;
+		goto exit;
+	}
+	NLA_PUT_STRING(msg, DETOUR_A_MSG, "hello, kernel");
 
+	rc = nl_send_auto_complete(sk, msg);
+	if (!rc)
+		fprintf(stderr, "nl_send_auto_complete failed: %d\n", rc);
+
+nla_put_failure: // needed by NLA_PUT_X macros
 exit:
 	nl_socket_free(sk);
 	return rc;
