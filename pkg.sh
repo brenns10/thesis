19ed8ade7a8c495dcef7cc720f90ebc3fa8a9f1d
Stephen Brennan <stephen@brennan.io>
2017-05-16 19:53:14 -0400

    update pkg back to .tar and add "manifest" support

--- pkg.sh
+++ pkg.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/sh
 make -C src clean
 rm -f vm.tar
-tar cJf vm.tar.xz \
-	src/* \
-	etc/* \
-	tmp/* \
-	setup.sh
+
+FILES="src/** etc/** tmp/** setup.sh"
+tar cf vm.tar $FILES
+sha256sum $FILES vm.tar > sha256sums.txt
+
 echo Package created!
+echo Manifest in sha256sums.txt
