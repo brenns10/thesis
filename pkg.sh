2b9101d0da69afb95d16a9da28a2223434ee4778
Stephen Brennan <stephen@brennan.io>
2017-05-16 20:01:32 -0400

    more pkg changes

--- pkg.sh
+++ pkg.sh
@@ -3,8 +3,9 @@ make -C src clean
 rm -f vm.tar
 
 FILES="src/** etc/** tmp/** setup.sh"
-tar cf vm.tar $FILES
+tar cvf vm.tar $FILES
 sha256sum $FILES vm.tar > sha256sums.txt
 
+echo
 echo Package created!
 echo Manifest in sha256sums.txt
