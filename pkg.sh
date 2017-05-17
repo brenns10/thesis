14375290d61cc4ce1694a7a64ffd144d412c61d5
Stephen Brennan <stephen@brennan.io>
2017-05-16 20:08:41 -0400

    final pkg change

--- pkg.sh
+++ pkg.sh
@@ -4,7 +4,8 @@ rm -f vm.tar
 
 FILES="src/** etc/** tmp/** setup.sh"
 tar cvf vm.tar $FILES
-sha256sum $FILES vm.tar > sha256sums.txt
+sha256sum $FILES > sha256sums.txt
+tar -xOf vm.tar | sha256sum >> sha256sums.txt
 
 echo
 echo Package created!
