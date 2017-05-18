78d9674f28f80497194c2ecf4ca477cc602584cc
Stephen Brennan <stephen@brennan.io>
2017-05-18 10:14:25 -0400

    MOAR TRIALS

--- src/mn_experiment.sh
+++ src/mn_experiment.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
+TRIALS=60
 PARAMS=sym
-./experiment.py -t 20 --params $PARAMS --setup control
-./experiment.py -t 20 --params $PARAMS --setup nat
-./experiment.py -t 20 --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup control
+./experiment.py -t $TRIALS --params $PARAMS --setup nat
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn
 PARAMS=sym_lossy
-./experiment.py -t 20 --params $PARAMS --setup control
-./experiment.py -t 20 --params $PARAMS --setup nat
-./experiment.py -t 20 --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup control
+./experiment.py -t $TRIALS --params $PARAMS --setup nat
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn
 PARAMS=sym_delayed
-./experiment.py -t 20 --params $PARAMS --setup control
-./experiment.py -t 20 --params $PARAMS --setup nat
-./experiment.py -t 20 --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup control
+./experiment.py -t $TRIALS --params $PARAMS --setup nat
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn
 PARAMS=easy
-./experiment.py -t 20 --params $PARAMS --setup control
-./experiment.py -t 20 --params $PARAMS --setup nat
-./experiment.py -t 20 --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup control
+./experiment.py -t $TRIALS --params $PARAMS --setup nat
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn
 PARAMS=lossy
-./experiment.py -t 20 --params $PARAMS --setup control
-./experiment.py -t 20 --params $PARAMS --setup nat
-./experiment.py -t 20 --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup control
+./experiment.py -t $TRIALS --params $PARAMS --setup nat
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn
 PARAMS=delayed
-./experiment.py -t 20 --params $PARAMS --setup control
-./experiment.py -t 20 --params $PARAMS --setup nat
-./experiment.py -t 20 --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup control
+./experiment.py -t $TRIALS --params $PARAMS --setup nat
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn
