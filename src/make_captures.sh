10c3e96a1acaecb0aee68e295503f44b19e8a121
Stephen Brennan <stephen@brennan.io>
2018-02-14 13:14:01 -0800

    src/make_captures.sh didn't make captures

--- src/make_captures.sh
+++ src/make_captures.sh
@@ -2,25 +2,25 @@
 
 TRIALS=1
 PARAMS=sym
-./experiment.py -t $TRIALS --params $PARAMS --setup nat
-./experiment.py -t $TRIALS --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup nat --trace
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn --trace
 
 PARAMS=sym_lossy
-./experiment.py -t $TRIALS --params $PARAMS --setup nat
-./experiment.py -t $TRIALS --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup nat --trace
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn --trace
 
 PARAMS=sym_delayed
-./experiment.py -t $TRIALS --params $PARAMS --setup nat
-./experiment.py -t $TRIALS --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup nat --trace
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn --trace
 
 PARAMS=easy
-./experiment.py -t $TRIALS --params $PARAMS --setup nat
-./experiment.py -t $TRIALS --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup nat --trace
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn --trace
 
 PARAMS=lossy
-./experiment.py -t $TRIALS --params $PARAMS --setup nat
-./experiment.py -t $TRIALS --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup nat --trace
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn --trace
 
 PARAMS=delayed
-./experiment.py -t $TRIALS --params $PARAMS --setup nat
-./experiment.py -t $TRIALS --params $PARAMS --setup vpn
+./experiment.py -t $TRIALS --params $PARAMS --setup nat --trace
+./experiment.py -t $TRIALS --params $PARAMS --setup vpn --trace
