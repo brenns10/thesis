6eb3036d23af36b5974ba158520c0b7d6df43b7a
Stephen Brennan <stephen@brennan.io>
2018-02-14 09:44:20 -0800

    New data.

---
+++ data/2018-02-13-vpn-correction/README.md
@@ -0,0 +1,6 @@
+2018-02-13 VPN Correction
+=========================
+
+This data was created after I realized that my vpn configuration was actually
+performing encryption and signatures. I adjusted it properly, and have re-run
+experiments.
---
+++ data/2018-02-13-vpn-correction/plots.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Produce plots.
+"""
+
+from os.path import dirname, abspath
+import sys
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+import output as o
+
+import matplotlib.pyplot as plt
+
+# order preserved (thanks python 3.6...?)
+META = {
+    'easy': 'Core-limited',
+    'lossy': 'Core-limited with Loss',
+    'delayed': 'Core-limited with High Latency',
+    'sym': 'Symmetric',
+    'sym_lossy': 'Symmetric with Loss',
+    'sym_delayed': 'Symmetric with High Latency',
+}
+
+# order preserved
+SCEN = {
+    'control.mptcp': '1 Subflow',
+    'nat.mptcp': 'NAT',
+    'vpn.mptcp': 'VPN',
+    'control.vanilla': 'TCP',
+    'nat.vanilla': 'TCP (NAT)',
+    'vpn.vanilla': 'TCP (VPN)',
+}
+
+
+def make_single_plot(base):
+    data = []
+    labels = []
+    text = META[base]
+    for s, label in SCEN.items():
+        arr = o.get_numpy_array('%s.%s.json' % (base, s)) / 1000000
+        data.append(arr)
+        text += ' & %.2f (%.2f)' % (arr.mean(), arr.std())
+        labels.append(label)
+
+    print(text + r' \\')
+
+    fig, ax = plt.subplots()
+    ax.boxplot(data, labels=labels)
+    ax.set_ylabel('Throughput (Mbps)')
+    ax.set_title('Throughput Comparison: ' + META[base])
+    fig.savefig('%s.pdf' % base.replace('.', '-').replace('_', '-'))
+
+
+def make_all_plots():
+    title = ''
+    for v in SCEN.values():
+        title += r' & \textbf{%s}' % v
+    print(title + r'\\')
+    for base in META.keys():
+        make_single_plot(base)
+
+
+def make_time_series_grid(base):
+    print(base)
+    rows = ['mptcp', 'vanilla']
+    cols = ['control', 'nat', 'vpn']
+    fig, axarr = plt.subplots(len(rows), len(cols), sharex=True, sharey=True)
+    for ridx, row in enumerate(rows):
+        for cidx, col in enumerate(cols):
+            data = o.get_time_series_arrays('%s.%s.%s.json' % (base, col, row))
+            for series in data:
+                axarr[ridx][cidx].plot(series[:10])
+    axarr[0][0].set_ylabel('MPTCP')
+    axarr[1][0].set_ylabel('TCP')
+    axarr[1][0].set_xlabel('Control')
+    axarr[1][1].set_xlabel('NAT')
+    axarr[1][2].set_xlabel('VPN')
+    fig.savefig('timegrid-%s.pdf' % base.replace('.', '-').replace('_', '-'))
+
+
+def make_all_time_series_grid():
+    for base in META.keys():
+        make_time_series_grid(base)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'time':
+        make_all_time_series_grid()
+    else:
+        make_all_plots()
